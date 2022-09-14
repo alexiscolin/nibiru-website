72a226d7cb1f52d1cafd4302de85cdeccfa18965
Unique-Divine <realuniquedivine@gmail.com>
2022-09-14 15:28:24 -0500

    deploy #wip render fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false, //https://reactjs.org/docs/strict-mode.html#ensuring-reusable-state
+  assetPrefix: './',
   swcMinify: true,
   images: {
     formats: ['image/avif', 'image/webp'],
